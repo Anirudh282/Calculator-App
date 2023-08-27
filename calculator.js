68ec7ebe60d008dfe50aae7481b4255152aaa0c0
Anirudh282 <anirudh282sharma@gmail.com>
2023-08-28 02:04:29 +0530

    JavaScript

---
+++ calculator.js
@@ -0,0 +1,17 @@
+document.querySelectorAll("button").forEach(button => {
+    button.addEventListener("click", function() {
+        if (this.textContent === "=") {
+                output.value = eval(output.value);
+            }
+            else 
+            {
+                output.value += this.textContent;
+            }
+});
+});
+document.getElementById("C").addEventListener("click", function() {
+    document.getElementById("output").value="";    
+});
+document.getElementById("Del").addEventListener("click", function() {
+    document.getElementById("output").value= document.getElementById("output").value.slice(0,-1);    
+});
